80ed2425b57f3826d7c304a4929c53dc409b7b71
nidha <nidhamuhammedali@gmail.com>
2019-12-28 11:38:10 +0530

    1 nidha   edit fragment

--- app/src/main/java/com/example/chcenter/Main2Activity.kt
+++ app/src/main/java/com/example/chcenter/Main2Activity.kt
@@ -1,20 +1,17 @@
 package com.example.chcenter
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.viewpager.widget.ViewPager
 import com.example.chcenter.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.fragment_container)
         viewPager.adapter = sectionsPagerAdapter
---
+++ app/src/main/java/com/example/chcenter/api/ApiInterface.kt
@@ -0,0 +1,4 @@
+package com.example.chcenter.api
+
+interface ApiInterface {
+}
